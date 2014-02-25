e00b3a2b0d28219bee33e231258b66fd8d0ea26d
sharonranels <sharonranels@yahoo.com>
2014-02-25 10:21:36 -0600

    final version after review

--- public/todo-list.php
+++ public/todo-list.php
@@ -1,40 +1,39 @@
-		<?php
+<?php
 
-		$items = array();
+$items = array();
 
-		
-		function open_file($input) {
-	        $handle = fopen($input, "r");
-	        $contents = fread($handle, filesize($input));
-	        fclose($handle);
-	        return explode("\n", $contents);
-	        }
+$filename = "todo_list.txt";
 
+function save_file($input, $items) {
+    $itemStr = implode("\n", $items);
+    $handle = fopen("todo_list.txt", "w+");
+    fwrite($handle, $itemStr);
+    fclose($handle);
+}
 
-	    function save_file($input, $items) {
-		    $itemStr = implode("\n", $items);
-		    $handle = fopen("todo_list.txt", "w+");
-		    fwrite($handle, $itemStr);
-		    fclose($handle);
-	    }
+function open_file($input) {
+    $handle = fopen($input, "r");
+    $contents = fread($handle, filesize($input));
+    fclose($handle);
+    return explode("\n", $contents);
+ }
+if (filesize($filename) > 0) {
+$items = open_file($filename);
+}
 
+if (isset($_POST['newitem']) && !empty($_POST['newitem'])) {
+	$item = $_POST['newitem'];
+	array_push($items, $item);
+	save_file("todo_list.txt", $items);
+}
 
-		$filename = "todo_list.txt";
-		$items = open_file($filename);
-		
-		if (isset($_POST['newitem'])) {
-			$item = $_POST['newitem'];
-			array_push($items, $item);
-			save_file("todo_list.txt", $items);
-		}
-
-		if (isset($_GET['remove'])) {
-			$item = $_GET['remove'];
-			unset($items[$item]);
-			save_file($filename, $items);
-			header("Location:todo-list.php");
-			exit(0);
-		}
+if (isset($_GET['remove'])) {
+	$item = $_GET['remove'];
+	unset($items[$item]);
+	save_file($filename, $items);
+	header("Location:todo-list.php");
+	exit(0);
+}
 ?>
 
 
@@ -63,7 +62,7 @@
 	<form method="POST" action="todo-list.php">
         <p>
             <label for="newitem">Please enter a new task to complete: </label>
-            <input id="newitem" name="newitem" type="text" placeholder="Type new task here...">
+            <input id="newitem" name="newitem" type="text" autofocus="autofocus" placeholder="Type new task here...">
         </p>
             
         <p>
