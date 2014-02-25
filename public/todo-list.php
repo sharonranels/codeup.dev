d5872d23e69f66e0b30fcb827b733327a29a48e0
sharonranels <sharonranels@yahoo.com>
2014-02-25 12:40:46 -0600

    files merged

--- public/todo-list.php
+++ public/todo-list.php
@@ -16,15 +16,35 @@ function open_file($input) {
     $contents = fread($handle, filesize($input));
     fclose($handle);
     return explode("\n", $contents);
- }
-if (filesize($filename) > 0) {
-$items = open_file($filename);
+	}
+		if (filesize($filename) > 0) {
+			$items = open_file($filename);
 }
+
+
+if(count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+	$upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
+	$filename = basename($_FILES['file1']['name']);
+	$saved_filename = $upload_dir . $filename;
+	move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+	$new_array = open_file($saved_filename);
+	$items = array_merge($items, $new_array);
+	$itemStr = implode("\n", $items);
+    $handle = fopen("todo_list.txt", "w+");
+    fwrite($handle, $itemStr);
+    fclose($handle);
+    header("Location:todo-list.php");
+	exit(0);
+}
+
+
 
 if (isset($_POST['newitem']) && !empty($_POST['newitem'])) {
 	$item = $_POST['newitem'];
 	array_push($items, $item);
 	save_file("todo_list.txt", $items);
+	header("Location:todo-list.php");
+	exit(0);
 }
 
 if (isset($_GET['remove'])) {
@@ -59,16 +79,32 @@ if (isset($_GET['remove'])) {
 	</ul>
 
 <h2>Add Tasks to Your TODO List</h2>
-	<form method="POST" action="todo-list.php">
-        <p>
-            <label for="newitem">Please enter a new task to complete: </label>
-            <input id="newitem" name="newitem" type="text" autofocus="autofocus" placeholder="Type new task here...">
-        </p>
-            
-        <p>
-            <button type="submit">Send</button>
-        </p>
-    </form>
+	
+<form method="POST" action="todo-list.php">
+    <p>
+        <label for="newitem">Please enter a new task to complete: </label>
+        <input id="newitem" name="newitem" type="text" autofocus="autofocus" placeholder="Type new task here...">
+    </p>
+
+    <p>
+        <button type="submit">Send</button>
+    </p>
+
+</form>
+
+    
+
+<form method="POST" enctype="multipart/form-data" action="">
+    <p>
+        <label for="file1">Please select file to upload: </label>
+        <input type="file" id="file1" name="file1">
+    </p>
+        
+    <p>
+        <button type="submit" value="Upload">Send</button>
+    </p>
+
+</form>
 
 
 
