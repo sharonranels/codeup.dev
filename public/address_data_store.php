4fadd6b9b3993634a59e6f27f2b9e4bdd4757f64
vagrant <vagrant@quantal.codeup.dev>
2014-03-01 11:57:29 -0600

    this file is the parent for the address book code

--- public/address_data_store.php
+++ public/address_data_store.php
@@ -1,35 +1,44 @@
 <?php
 
-class AddressDataStore {
+require_once('filestore.php');
 
-	public $filename = '';
+class AddressDataStore extends Filestore {
 
-	function __construct($filename = '')
-	{
-		$this->filename = $filename;
-	}
 
-    function read_address_book()
-    {
-        if(empty($this->filename)) {
-    	$address_book = [];
-    } else {
-    	$handle = fopen($this->filename, "r+");
-    	while(($data = fgetcsv($handle)) !==FALSE) {
-    		$address_book[] = $data;
-		}	
-    	fclose($handle);
-	}
-    	return $address_book;
-    }
+	// public $filename = '';
 
-    function writeCSV($addresses_array) 
-    {
-        $handle = fopen($this->filename, 'w');
-	foreach ($addresses_array as $row) {
-		fputcsv($handle, $row);
-	}
-	fclose($handle);
-	}
+	// function __construct($filename = '')
+	// {
+	// 	$this->filename = $filename;
+	// }
+
+
+
+
+ //    function read_address_book()
+ //    {
+ //        if(empty($this->filename)) {
+ //    	$address_book = [];
+ //        } else {
+ //    	$handle = fopen($this->filename, "r+");
+ //    	while(($data = fgetcsv($handle)) !==FALSE) {
+ //    		$address_book[] = $data;
+	// 	}	
+ //    	fclose($handle);
+ //        }
+ //        return $address_book;
+ //    }
+
+ 
+
+
+ //    function writeCSV($addresses_array) 
+ //    {
+ //        $handle = fopen($this->filename, 'w');
+ //    	foreach ($addresses_array as $row) {
+ //    		fputcsv($handle, $row);
+ //    	}
+ //    	fclose($handle);
+	// }
 }
 ?>
